1035fef50292468ac6ac036be47c820b2298f9de
Riya-Raj-android <riya511raj@gmail.com>
2026-06-21 10:01:04 +0530

    Update data_cleaning.sql

--- data_cleaning.sql
+++ data_cleaning.sql
@@ -24,17 +24,6 @@ row_number() over(
  as row_num
 from layoffs_staging;
 
-with duplicates_cte as
- (
-   select *,
-row_number() over(
- partition by company,location,industry,total_laid_off,percentage_laid_off,`date`,stage,country,funds_raised_millions)
- as row_num
-from layoffs_staging
-)
-select *
-from duplicates_cte
-where row_num>1;
 
 select * from layoffs_staging 
 where company='Casper';
@@ -55,8 +44,8 @@ from duplicates_cte
 where row_num>1;
 
 
--- drop table layoffs_staging2;
 
+-- created new table so that the original table should not get affected in order to get data from it whener needed.
 CREATE TABLE `layoffs_staging2` (
   `company` text,
   `location` text,
@@ -104,31 +93,6 @@ from layoffs_staging
 select *
 from duplicates_cte
 where row_num>1;
-
-CREATE TABLE `layoffs_staging2` (
-  `company` text,
-  `location` text,
-  `industry` text,
-  `total_laid_off` int DEFAULT NULL,
-  `percentage_laid_off` text,
-  `date` text,
-  `stage` text,
-  `country` text,
-  `funds_raised_millions` int DEFAULT NULL,
-  `row_num` INT 
-  
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-select * from layoffs_staging2;
-
-insert into layoffs_staging2 
- select *,
-row_number() over(
- partition by 
- company,location,industry,total_laid_off,percentage_laid_off,`date`,
- stage,country,funds_raised_millions)
- as row_num
-from layoffs_staging;
 
 select * from layoffs_staging2 ;
 
