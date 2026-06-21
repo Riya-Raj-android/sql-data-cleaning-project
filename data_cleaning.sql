0c023e19df00e5b2a798d97655c7655cd7bab099
Riya-Raj-android <riya511raj@gmail.com>
2026-06-21 09:45:50 +0530

    Create data_cleaning.sql

---
+++ data_cleaning.sql
@@ -0,0 +1,263 @@
+-- Data Cleaning
+select * from layoffs;
+select count(*) from layoffs;
+-- 1 REMOVE DUPLICATES
+-- 2 STANDARDIZE DATA
+-- 3 NULL VALUES/BLANK VALUES
+-- 4 REMOVE UNECESSARY ROWS /COLUMNS
+
+create table layoffs_staging
+like layoffs;
+select * from layoffs_staging;
+-- 1 REMOVE DUPLICATES
+insert layoffs_staging 
+select * from layoffs;
+
+select count(*) from layoffs_staging;
+
+select * from layoffs_staging;
+
+-- 1 REMOVE DUPLICATES
+select *,
+row_number() over(
+ partition by company,industry,total_laid_off,percentage_laid_off,`date`)
+ as row_num
+from layoffs_staging;
+
+with duplicates_cte as
+ (
+   select *,
+row_number() over(
+ partition by company,location,industry,total_laid_off,percentage_laid_off,`date`,stage,country,funds_raised_millions)
+ as row_num
+from layoffs_staging
+)
+select *
+from duplicates_cte
+where row_num>1;
+
+select * from layoffs_staging 
+where company='Casper';
+
+with duplicates_cte as
+ (
+   select *,
+row_number() over(
+ partition by 
+ company,location,industry,total_laid_off,
+ percentage_laid_off,`date`,stage,country,
+ funds_raised_millions)
+ as row_num
+from layoffs_staging
+)
+select *
+from duplicates_cte
+where row_num>1;
+
+
+-- drop table layoffs_staging2;
+
+CREATE TABLE `layoffs_staging2` (
+  `company` text,
+  `location` text,
+  `industry` text,
+  `total_laid_off` int DEFAULT NULL,
+  `percentage_laid_off` text,
+  `date` text,
+  `stage` text,
+  `country` text,
+  `funds_raised_millions` int DEFAULT NULL,
+  `row_num` INT 
+  
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+select * from layoffs_staging2;
+
+insert into layoffs_staging2 
+ select *,
+row_number() over(
+ partition by 
+ company,location,industry,total_laid_off,percentage_laid_off,`date`,
+ stage,country,funds_raised_millions)
+ as row_num
+from layoffs_staging;
+select count(*) from layoffs_staging2;
+
+select *
+from duplicates_cte
+where row_num>1;
+
+select * from layoffs_staging 
+where company='Casper';
+
+with duplicates_cte as
+ (
+   select *,
+row_number() over(
+ partition by 
+ company,location,industry,total_laid_off,
+ percentage_laid_off,`date`,stage,country,
+ funds_raised_millions)
+ as row_num
+from layoffs_staging
+)
+select *
+from duplicates_cte
+where row_num>1;
+
+CREATE TABLE `layoffs_staging2` (
+  `company` text,
+  `location` text,
+  `industry` text,
+  `total_laid_off` int DEFAULT NULL,
+  `percentage_laid_off` text,
+  `date` text,
+  `stage` text,
+  `country` text,
+  `funds_raised_millions` int DEFAULT NULL,
+  `row_num` INT 
+  
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+select * from layoffs_staging2;
+
+insert into layoffs_staging2 
+ select *,
+row_number() over(
+ partition by 
+ company,location,industry,total_laid_off,percentage_laid_off,`date`,
+ stage,country,funds_raised_millions)
+ as row_num
+from layoffs_staging;
+
+select * from layoffs_staging2 ;
+
+select * from layoffs_staging2 
+where row_num >1;
+
+delete
+ from layoffs_staging2 
+where row_num >1;
+
+select count(*) from layoffs_staging2;
+
+select * from layoffs_staging2 
+where row_num >1;
+
+
+-- standardizing data
+
+select company,trim(company)
+from layoffs_staging2 ;
+
+update layoffs_staging2 
+set company=trim(company);
+
+select distinct industry
+from layoffs_staging2 order by 1 ;
+
+select * from layoffs_staging2
+where industry like 'crypto%';
+
+update layoffs_staging2
+set industry='Crypto'
+where industry like 'crypto%';
+
+select distinct industry
+from layoffs_staging2;
+
+select * from layoffs_staging2;
+
+select distinct country
+from layoffs_staging2 order by 1 ;
+
+select *
+from layoffs_staging2 where country like 'United States%' order by 1;
+
+select distinct country,trim(trailing '.' from country)
+from layoffs_staging2 order by 1 ;
+
+update layoffs_staging2
+set country=trim(trailing '.' from country)
+where country like 'United States%';
+
+select * from layoffs_staging2;
+
+select `date`,
+str_to_date(`date`,'%m/%d/%Y') 
+from layoffs_staging2;
+
+update layoffs_staging2
+set `date` = str_to_date(`date`,'%m/%d/%Y') ;
+
+select `date`
+from layoffs_staging2;
+
+select *
+from layoffs_staging2;
+
+alter table layoffs_staging2
+modify column `date` DATE;
+
+-- 3 NULL VALUES/BLANK VALUES
+select *
+from layoffs_staging2 
+where total_laid_off is NULL
+and percentage_laid_off is NULL  ;
+
+update layoffs_staging2 
+set industry=null
+where industry='';
+
+-- select  distinct industry from 
+-- layoffs_staging2;
+
+-- select count(*) from layoffs_staging2;
+
+select  *
+from layoffs_staging2 
+where industry is null 
+or industry ='';
+
+select * 
+from layoffs_staging2
+where company='Airbnb';
+
+select t1.industry,t2.industry
+from layoffs_staging2 t1
+join layoffs_staging2 t2
+on t1.company=t2.company
+and t1.location=t2.location
+where (t1.industry is null or t1.industry='')
+and t2.industry is not null;
+
+
+update layoffs_staging2 t1
+join layoffs_staging2 t2
+  on t1.company=t2.company
+set t1.industry=t2.industry
+where t1.industry is null
+and t2.industry is not null;
+
+select  *
+from layoffs_staging2 
+where company like 'Bally%';
+
+select * from layoffs_staging2;
+
+select *
+from layoffs_staging2 
+where total_laid_off is NULL
+and percentage_laid_off is NULL  ;
+
+delete
+from layoffs_staging2 
+where total_laid_off is NULL
+and percentage_laid_off is NULL  ;
+-- select count(*) from layoffs_staging2;
+
+-- REMOVE ROW_NUM COLUMN NOW
+alter table layoffs_staging2
+drop column row_num;
+
+select * from layoffs_staging2;
